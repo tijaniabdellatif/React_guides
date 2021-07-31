7ab557fa928dabae79927fea769da3da624e7cc8
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2021-07-31 01:33:44 +0100

    'component_in_details'

--- src/index.js
+++ src/index.js
@@ -15,9 +15,47 @@ import ReactDom from 'react-dom';
  */
 const Greeting = () => {
 
-return <h4>this is first component</h4>
+return (
+    <div className="heading">
+        <Heading />
+        <h4>this is first component</h4>
+        <Heating />
+    </div>
+    );
 
 };
+
+const Heating = () => {
+
+    return React.createElement('div',{},React.createElement('h1',{},'A title inside a div'));
+
+};
+
+const Heading = () => {
+
+    return(
+<React.Fragment>
+    <section>
+
+        <article>
+            <h3>Logo</h3>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </article>
+    </section>
+</React.Fragment>
+        );
+
+}
 
 /**
  * Rendering the component
